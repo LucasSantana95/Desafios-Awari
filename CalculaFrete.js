cdc85ec23660c0024d04bd62511a09f0f4ac30a8
LucasSantana95 <lucassg1995@gmail.com>
2022-10-10 20:03:28 -0300

    Add files via upload

---
+++ CalculaFrete.js
@@ -0,0 +1,8 @@
+let nomeProduto = "Camiseta da Awari";
+let enderecoProduto = "Fortaleza - CE";
+
+function calculaFrete(nomeProduto,enderecoProduto){
+    console.log("Olá, boas vindas à nossa Loja. Já recebemos as informações e iremos mandar o produto " + nomeProduto + " para " + enderecoProduto);
+}
+
+calculaFrete(nomeProduto,enderecoProduto);
